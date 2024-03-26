ed9f3f723420235d47da9c5886552a7fc25ac99a
scziskh <muzychukserhii@gmail.com>
2024-03-26 19:52:33 +0200

    Updates

---
+++ static/AQRbY58JQ1kuTpJkxUjgN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-42cfd8c0eb9b7b72.js"],"/404":["static/chunks/pages/404-0119d825e95ab167.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ static/chunks/pages/404-0119d825e95ab167.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{394:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(2091)}])},2091:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return i}});var t=_(5893),i=!0;u.default=()=>(0,t.jsx)("div",{})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=394)}),_N_E=n.O()}]);
